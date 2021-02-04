33620df0f413cb7c63904edb46e66b252e376407
Ali Sirait <52443542+Alisirait@users.noreply.github.com>
2021-02-05 00:39:11 +0700

    commit ali sumberdata jawa timur tengah barat

--- app/src/main/java/com/cindy/food/jawatenggah/SumberData.kt
+++ app/src/main/java/com/cindy/food/jawatenggah/SumberData.kt
@@ -7,68 +7,135 @@ class SumberData {
         fun buatSetData(): ArrayList<ListObjResep> {
             val list = ArrayList<ListObjResep>()
             list.add(
-                    ListObjResep(
-                            "Rabeg Sapi Banten",
-                            "Wadah\n" +
-                                    "Panci",
-                            "https://img-global.cpcdn.com/recipes/3a01d5de7fdd2f67/751x532cq70/rabeg-sapi-banten-foto-resep-utama.webp",
-                            "500 gram daging sapi\n" +
-                                    "1 batang serai\n" +
-                                    "2 lembar daun salam\n" +
-                                    "1 ruas kayu manis\n" +
-                                    "5 sdm gula aren\n" +
-                                    "1 sdm asam jawa matang\n" +
-                                    "1 sdm kecap manis\n" +
-                                    "2secukupnya garam\n" +
-                                    "secukupnya lada\n" +
-                                    "secukupnya kaldu bubuk\n" +
-                                    "secukupnya minyak goreng\n" +
-                                    "secukupnya air\n" +
-                                    "Bumbu Halus\n" +
-                                    "5 siung bawang merah\n" +
-                                    "5 siung bawang putih\n" +
-                                    "4 buah cabai merah keriting\n" +
-                                    "cabai rawit (sesuai selera, me : 20 buah)\n" +
-                                    "1 buah tomat (me : ukuran sedang)\n" +
-                                    "5 butir kemiri (sangrai)\n" +
-                                    "1 ruas jahe\n" +
-                                    "1 ruas kunyit\n" +
-                                    "1 ruas lengkuas\n" +
-                                    "1 sdm ketumbar bubuk\n",
-                            "Langkah\n" +
-                                    "1. Perlakukan daging sapi seperti pada tips terlampir agar daging empuk. Lalu potong-potong sesuai selera. \n" +
-                                    "2. Haluskan seluruh bahan bumbu halus.\n" +
-                                    "3. Siapkan juga daun salam dan serai.\n" +
-                                    "4. Tumis bumbu halus bersama dengan serai dan daun salam. Tumis hingga harum dan warna bumbu menjadi lebih gelap.\n" +
-                                    "5. Setelah bumbu harum, masukkan daging sapi, aduk aduk hingga daging sedikit menyusut.\n" +
-                                    "6. Tambahkan kurang lebih 3 - 4 gelas air, bumbui dengan garam, lada dan kaldu bubuk. Masak kembali dengan api kecil hingga airnya sedikit menyusut untuk mematangkan daging.\n" +
-                                    "7. Sambil menunggu air pada daging menyusut, sisir gula aren (jumlah yang difoto hanya ± 1 sdm, tapi sebenarnya sewaktu tes rasa, saya tambahkan kembali gula aren sesuai takaran pada daftar bahan).\n" +
-                                    "8. Larutkan asam jawa matang dalam ± 3 sdm air.\n" +
-                                    "9. Setelah air pada daging mulai menyusut, tambahkan gula aren yang sudah disisir. Tambahkan pula air asam jawa, kecap manis dan kayu manis. Masak hingga bumbu menyusut sambil sesekali diaduk agar tidak hangus.\n"  +
-                                    "10. Selesai, siap dinikmati.\n"
-
-                    )
+                ListObjResep(
+                    "Brekecek ikan",
+                    "Wadah\n" +
+                            "Panci",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/21/172627/1114523-15-resep-makanan-khas-jawa-tengah-terkenal-enak-dan-mudah.jpg",
+                    "Bahan:\n" +
+                            "- 1 batang sereh\n" +
+                            "- 2 lembar daun jeruk\n" +
+                            "- 1 tangkai daun bawang\n" +
+                            "- 3 lembar daun salam\n" +
+                            "- gula merah secukupnya\n" +
+                            "- 1 tomat\n" +
+                            "- 2 cabai merah besar\n" +
+                            "- Royko sapi\n" +
+                            "- air secukupnya\n" +
+                            "\n" +
+                            "Bumbu halus:\n" +
+                            "- 1 sdt garam\n" +
+                            "- 3 biji kemiri\n" +
+                            "- 7 cabai rawit\n" +
+                            "- 1 cabai merah\n" +
+                            "- 4 siung bawang merah\n" +
+                            "- sejumput lada\n" +
+                            "- 3 siung bawang putih\n" +
+                            "- 2 cm kunyit\n" +
+                            "- 2 cm jahe\n",
+                    "Cara memasak:\n" +
+                            "a. Goreng ikan. Sisihkan\n" +
+                            "b. Sangrai kemiri, kunyit, bawang putih, dan jahe.\n" +
+                            "c. Tumis bumbu halus, masukan gula merah dan tomat. Aduk hingga bumbu matang. Tuang air secukupnya.\n" +
+                            "d. Masukkan daun jeruk, daun bawang, irisan cabai merah besar, dan daun bawang.\n" +
+                            "e. Masukan ikan, aduk sampai mendidih. Tambahkan Royko sapi.\n"
+                )
             )
             list.add(
-                    ListObjResep(
-                            "Tumis Kulit Melinjo Khas Banten",
-                            "Kompor",
-                            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.suara.com%2Flifestyle%2F2019%2F07%2F31%2F070000%2Ftumis-kulit-melinjo-teri-lembut-dan-gurih-kreasi-yang-bikin-makan-lahap&psig=AOvVaw35YhplqIqyM4g4mRZYVXAH&ust=1612027171925000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJj45KrTwe4CFQAAAAAdAAAAABAD",
-                            "1 bungkus kulit melinjo\n" +
-                                    "segenggam teri nasi\n" +
-                                    "7 buah cabe hijau (atau sesuai selera)\n" +
-                                    "3 sdm minyak sayur\n" +
-                                    "3 buah bawang putih\n" +
-                                    "5 buah bawah merah\n" +
-                                    "3 lembar daun jeruk\n" +
-                                    "1 ruas lengkuas (geprek)\n" +
-                                    "100 ml air\n" +
-                                    "secukupnya garam, gulput, merica bubuk dan penyedap\n",
-                            "1. Cuci bersih kulit melinjo, belah 2 memanjang atau sesuai selera. Lalu rebus kurleb 5 menit supaya tidak langu. Angkat dan tiriskan.\n" +
-                                    "2. Tumis teri yg sdh dicuci bersih (jangan terlalu kering), tambahkan irisan bawang merah, bawang putih, daun jeruk, lengkuas geprek dan cabe hijau yg sdh dipotong2.\n" +
-                                    "3. Masukkan kulit melinjo, aduk rata. Tambahkan air, garam, gulput, merica bubuk dan penyedap.\n" +
-                                    "4. Cek rasa, tumis kulit melinjo khas Banten siap dihidangkan."
-                    )
+                ListObjResep(
+                    "Soto Kudus",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/21/172627/1114525-1000xauto-15-resep-makanan-khas-jawa-tengah-terkenal-enak-dan-mudah.jpg",
+                    "Bahan:\n" +
+                            "- 1 ekor ayam kampung, potong-potong\n" +
+                            "- 3 lembar daun jeruk\n" +
+                            "- 2 sdm gula merah, sisir\n" +
+                            "- air secukupnya\n" +
+                            "- 2 batang serai, memarkan\n" +
+                            "- 3 lembar daun salam\n" +
+                            "- 2 batang daun bawang, potong-potong\n" +
+                            "- garam dan merica bubuk secukupnya\n" +
+                            "\n" +
+                            "Bumbu halus:\n" +
+                            "- 5 siung bawang putih\n" +
+                            "- 2 ruas kunyit\n" +
+                            "- 1/2 sdt ketumbar\n" +
+                            "- 10 siung bawang merah\n" +
+                            "- 4 butir kemiri\n" +
+                            "- 2 ruas jahe\n" +
+                            "- sejumput jinten\n" +
+                            "\n" +
+                            "Bahan pelengkap:\n" +
+                            "- tauge rebus\n" +
+                            "- sambal rawit\n" +
+                            "- bawang goreng\n" +
+                            "- emping atau keripik kentang\n" +
+                            "- seledri, iris tipis\n" +
+                            "- jeruk nipis\n",
+                    "Cara memasak:\n" +
+                            "a. Rebus ayam sampai matang, tiriskan. Ganti air, rebus lagi ayam hingga empuk.\n" +
+                            "b. Tumis bumbu halus, masukkan daun salam, daun bawang, daun jeruk, dan serai.\n" +
+                            "c. Tuang 2 sendok sayur kaldu ayam, beri daun bawang. Masak lagi hingga mendidih.\n" +
+                            "d. Masukkan tumisan ke panci ayam yang sedang direbus, aduk rata. Beri gula merah, garam, dan merica bubuk secukupnya.\n" +
+                            "e. Tiriskan ayam, suwir-suwir secukupnya.\n" +
+                            "f. Tata nasi dengan suwiran ayam dan tauge rebus, tuang kuah soto kudus. Beri bahan pelengkap di atasnya.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Nasi gandul",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/21/172627/1114522-1000xauto-15-resep-makanan-khas-jawa-tengah-terkenal-enak-dan-mudah.jpg",
+                    "Bahan:\n" +
+                            "- 500 gram daging sapi\n" +
+                            "- 2 batang serai\n" +
+                            "- garam dan kecap manis secukupnya\n" +
+                            "- 2 cm lengkuas\n" +
+                            "- 2 lembar salam\n" +
+                            "- jeruk nipis\n" +
+                            "- kaldu jamur secukupnya\n" +
+                            "- 1 sdm gula jawa\n" +
+                            "- 1,5 liter santan kelaoa\n" +
+                            "- bawang goreng\n" +
+                            "\n" +
+                            "Bumbu halus:\n" +
+                            "- 10 butir bawang merah\n" +
+                            "- 6 butir kemiri\n" +
+                            "- 1/2 sdt merica bubuk\n" +
+                            "- 4 siung bawang putih\n" +
+                            "- 2 buah kluwak\n" +
+                            "- 1/4 sdt jinten\n" +
+                            "- 2 cm kencur\n" +
+                            "- 1/2 sdt ketumbar sangrai\n",
+                    "Cara memasak:\n" +
+                            "a. Rebus daging sampai empuk.\n" +
+                            "b. Tumis bumbu halus, masukkan serai, lengkuas, dan daun salam.\n" +
+                            "c. Masukkan daging, masak hingga daging berubah warna.\n" +
+                            "d. Tuang air 2 liter dan santan kelapa.\n" +
+                            "e. Beri gula jawa, garam, kaldu jamur, dan kecap manis. Masak dengan api kecil hingga bumbu meresap.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Garang asem",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/21/172627/1114521-1000xauto-15-resep-makanan-khas-jawa-tengah-terkenal-enak-dan-mudah.jpg",
+                    "Bahan:\n" +
+                            "- 1 kg ayam\n" +
+                            "- 5 siung bawang putih, geprek halus\n" +
+                            "\n" +
+                            "Bumbu:\n" +
+                            "- 4 buah cabe ijo plompong, potong-potong\n" +
+                            "- 2 lembar daun salam\n" +
+                            "- 7 buah cabe rawit\n" +
+                            "- 5 buah tomat hijau, potong-potong\n" +
+                            "- 3 cm lengkuas\n" +
+                            "- santan cair secukupnya\n",
+                    "Cara memasak:\n" +
+                            "a. Rebus ayam hingga cukup empuk.\n" +
+                            "b. Masukkan semua bumbu. Masak hingga ayam setengah matang.\n" +
+                            "c. Tuang santan cair, beri gula dan garam secukupnya. Masak sampai matang dan siap disajikan.\n"
+                )
             )
             return list
         }
