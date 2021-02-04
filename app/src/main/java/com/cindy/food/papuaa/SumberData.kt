be74a72ffd6acb9a4baf467d58a464ccf44dce21
Ali Sirait <52443542+Alisirait@users.noreply.github.com>
2021-02-04 22:05:30 +0700

    commit ali sumberdata papua dan papuabarat

--- app/src/main/java/com/cindy/food/papuaa/SumberData.kt
+++ app/src/main/java/com/cindy/food/papuaa/SumberData.kt
@@ -15,7 +15,6 @@ class SumberData {
                             "Bahan papeda:\n" +
                                     "- 250 gr tepung sagu\n" +
                                     "- 500 ml air mendidih\n" +
-                                    "\n" +
                                     "Bahan tuna kuah kuning:\n" +
                                     "- 500 gr tuna fillet\n" +
                                     "- daun kemangi secukupnya\n" +
@@ -49,24 +48,156 @@ class SumberData {
             )
             list.add(
                     ListObjResep(
-                            "Tumis Kulit Melinjo Khas Banten",
+                            "Ikan bungkus",
                             "Kompor",
-                            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.suara.com%2Flifestyle%2F2019%2F07%2F31%2F070000%2Ftumis-kulit-melinjo-teri-lembut-dan-gurih-kreasi-yang-bikin-makan-lahap&psig=AOvVaw35YhplqIqyM4g4mRZYVXAH&ust=1612027171925000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJj45KrTwe4CFQAAAAAdAAAAABAD",
-                            "1 bungkus kulit melinjo\n" +
-                                    "segenggam teri nasi\n" +
-                                    "7 buah cabe hijau (atau sesuai selera)\n" +
-                                    "3 sdm minyak sayur\n" +
-                                    "3 buah bawang putih\n" +
-                                    "5 buah bawah merah\n" +
-                                    "3 lembar daun jeruk\n" +
-                                    "1 ruas lengkuas (geprek)\n" +
-                                    "100 ml air\n" +
-                                    "secukupnya garam, gulput, merica bubuk dan penyedap\n",
-                            "1. Cuci bersih kulit melinjo, belah 2 memanjang atau sesuai selera. Lalu rebus kurleb 5 menit supaya tidak langu. Angkat dan tiriskan.\n" +
-                                    "2. Tumis teri yg sdh dicuci bersih (jangan terlalu kering), tambahkan irisan bawang merah, bawang putih, daun jeruk, lengkuas geprek dan cabe hijau yg sdh dipotong2.\n" +
-                                    "3. Masukkan kulit melinjo, aduk rata. Tambahkan air, garam, gulput, merica bubuk dan penyedap.\n" +
-                                    "4. Cek rasa, tumis kulit melinjo khas Banten siap dihidangkan."
+                            "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119415-resep-makanan-khas-papua.jpg",
+                            "Bahan-bahan:\n" +
+                                    "- 500 gr ikan bandeng segar, potong-potong\n" +
+                                    "- 1 1/2 sdt garam\n" +
+                                    "- 1/2 sdt gula pasir\n" +
+                                    "- 2 buah cabai merah, potong bulat\n" +
+                                    "- 2 buah cabai hijau besar, potong bulat\n" +
+                                    "- 12 buah cabai rawit merah\n" +
+                                    "- 4 buah belimbing wuluh, potong-potong\n" +
+                                    "- 3 cm lengkuas, iris tipis-tipis\n" +
+                                    "- 1 batang serai, ambil bagian putihnya kemudian memarkan\n" +
+                                    "- 4 lembar daun salam\n" +
+                                    "- daun pisang secukupnnya untuk membungkus\n" +
+                                    "\n" +
+                                    "Bahan halus:\n" +
+                                    "- 8 butir bawang merah\n" +
+                                    "- 4 siung bawang putih\n" +
+                                    "- 2 buah cabai merah keriting\n" +
+                                    "- 1/4 sdt merica butiran\n" +
+                                    "- 1/4 sdt ketumbar, sangrai\n" +
+                                    "- 2 butir kemiri, sangrai\n",
+                            "Cara memasak:\n" +
+                                    "- Pertama haluskan semua bahan yang sudah disiapkan sebelumnya\n" +
+                                    "- Aduk rata ikan bandengnya dengan campuran bumbu halus, cabai merah, cabai hijau, cabai rawit, belimbing, lengkuas, dan serai\n" +
+                                    "- Lalu letakkan daun salam diatas 2 lembar daun pisang, kemudian letakkan adonan ikan bandengnya. Bungkus bentuk pepes, sematkan dengan lidi\n" +
+                                    "- Selanjutnya kukus dalam dandang panas diatas api sedang selama 45 menit hingga matang\n" +
+                                    "- Angkat dan sajikan.\n"
                     )
+            )
+            list.add(
+                ListObjResep(
+                    "Sambal colo-colo",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119419-1000xauto-resep-makanan-khas-papua.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 12 cabai merah, diiris kasar\n" +
+                            "- 6 butir bawang merah, diiris kasar\n" +
+                            "- 2 sdm kecap manis\n" +
+                            "- 1 sdm mentega cair\n" +
+                            "- 1 sdm jerus nipis\n",
+                    "Cara membuat:\n" +
+                            "- Campurkan semua bahan, lalu aduk sampai rata\n" +
+                            "- Hidangkan sambai sebagai pendamping ikan bakar dan juga lalapan sayur mentah, seperti terong, kacang panjang, daun kol dan daun selada.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Karaka (kepiting hitam Papua) kuah kuning",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119417-resep-makanan-khas-papua.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 4 ekor karaka ukuran sedang\n" +
+                            "- 4 siung bawang putih\n" +
+                            "- 5 butir bawang merah\n" +
+                            "- 500 ml santan cair\n" +
+                            "- 1 sdt garam\n" +
+                            "- 1 sdt penyedap rasa\n" +
+                            "- 1 sdt gula\n" +
+                            "- 1/2 sdt merica bubuk\n" +
+                            "- 1 ruas kunyit\n" +
+                            "- 2 buah kemiri\n" +
+                            "- 7 buah cabai rawit\n" +
+                            "- 1 buah tomat\n" +
+                            "- 1 batang sereh\n" +
+                            "- 1 ruas lengkuas\n" +
+                            "- 2 lembar daun salam\n" +
+                            "- 3 lembar daun jeruk\n" +
+                            "- 1 gelas air\n" +
+                            "- sedikit minyak untuk menumis\n",
+                    "Cara memasak:\n" +
+                            "- Cuci bersih karaka yang sudah digepuk menggunakan tang\n" +
+                            "- Haluskan bawang merah, bawang putih, cabai, kemiri tambahkan daun salam, daun jeruk, serem dan Lengkuas tumis hingga harum, tambahkan gula, garam, merica dan penyedap rasa\n" +
+                            "- Tambahkan air secukupnya aduk hingga mendidih, lalu masukan karaka yang sudah dibersihkan\n" +
+                            "- Masak karaka hingga air mendidih dan menyusut, setelah itu tambahkan santan cair aduk aduk hingga mendidih dan sedikit menyusut dan tambahkan potongan tomat agar lebih enak\n" +
+                            "- Sajikan.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Sagu Seb (Sagu bakar khas Papua)",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119418-resep-makanan-khas-papua.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 1/2 kg tepung sagu asli khusus untuk sagu bakar yang warna merah\n" +
+                            "- 3 buah kelapa setengah tua ambil dagingnya saja, boleh di keruk pakai sendok atau pengeruk kelapa\n" +
+                            "- Air secukupnya\n" +
+                            "- Tempurung kelapa secukupnnya.\n",
+                    "Cara membuat:\n" +
+                            "- Ambil satu wadah masukkan semua kelapa yang sudah di kerok ke dalam wadah\n" +
+                            "- Kemudian taburi kelapa dengan tepung sagu sedikit demi sedikit sambil di aduk-aduk sampai merata dan tepung sagu habis\n" +
+                            "- Untuk adonan sagu ini, adonannya tidak cair dan tidak boleh di tambah air kecuali kelapa dengan tepung tidak menempel. Kalaupun ditambah air, cukup dipercikkan saja ke dalam adonan\n" +
+                            "- Tekstur adonannya nanti akan seperti adonan membuat pie susu\n" +
+                            "- Bungkus adonan dengan daun pisang atau tata di dalam tempurung kelapa lalu masukkan ke dalam baru batu yang sebelumnya batu sudah dibakar\n" +
+                            "- Tutup dengan daun pisang kemudian di tambah tutup dengan batu-batu panas di atasnya\n" +
+                            "- Sajikan.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Ulat sagu",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119420-resep-makanan-khas-papua.jpg",
+                    "Bahan-bahan:\n" +
+                            "- ulat sagu secukupnya\n" +
+                            "- jeruk nipis sebanyak 1 buah, ambil airnya\n" +
+                            "- tusuk sate secukupnya\n" +
+                            "\n" +
+                            "Sambal kecap:\n" +
+                            "- 5 sdm kecap manis\n" +
+                            "- 1 butir bawang merah\n" +
+                            "- 2 siung bawang putih\n" +
+                            "- 5 buah cabai rawit merah\n",
+                    "Cara membuat sambal kecap:\n" +
+                            "- Pertama iris dahulu bawang merah, bawang putih serta cabai rawit sesuai selera dan masukan ke dalam wadah\n" +
+                            "- Tambahkan kecap manis dan aduk-aduk\n" +
+                            "\n" +
+                            "Cara membuat sate ulat sagu:\n" +
+                            "- Langkah pertama siapkan ulat sagunya. Ambil ulat sagu kemudian cuci dibawah air mengalir lalu tiriskan\n" +
+                            "- Masukan ulat sagu pada wadah bersih dan masukan air jeruk nipis kedalamnya\n" +
+                            "- Aduk-aduk hingga jeruk ulat berabalut air jeruk\n" +
+                            "- Ambil 1 buah tusuk sate dan tusukan beberapa ulat sagu. Lakukan sampai ulat sagu habis dan sisihkan sejenak\n" +
+                            "- Siapkan alat pembakaran berupa arang atau grill pan dan panaskan\n" +
+                            "- Bakar ulat sagu tadi diatas panggangan sampai matang. Jangan lupa untuk membolak-balik sesekali agar ulat sagu matang sempurna\n" +
+                            "- Jika sudah berwarna kuning keemasan segara angkat dan sisihkan\n" +
+                            "- Siap sajikan.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Ayam Baboto",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/30/173135/1119426-resep-makanan-khas-papua.png",
+                    "Bahan-bahan:\n" +
+                            "- 250 gr fillet ayam, dicincang halus\n" +
+                            "- 3 siung bawang merah\n" +
+                            "- 2 siung bawang putih\n" +
+                            "- 1/2 sdt kenari yang sudah dihaluskan\n" +
+                            "- 1 butir telur\n" +
+                            "- 1 sdm santan kental\n" +
+                            "- 1/2 sdt garam\n" +
+                            "- 1/4 sdt lada halus\n" +
+                            "- 1/4 sdt penyedap rasa\n" +
+                            "- 1/8 sdt pala bubuk\n" +
+                            "- daun pisang secukupnya untuk membungkus adonan.\n",
+                    "Cara membuat:\n" +
+                            "- Campur semua bahan. aduk sampai tercampur rata. bungkus dengan daun pisang. panaskan air kukusan sampai mendidih. " +
+                            "lalu kukus selama 45 menit dengan api sedang. sebelah matang, tiriskan, Dan kemudian siap disantap.\n"
+                )
             )
             return list
         }
