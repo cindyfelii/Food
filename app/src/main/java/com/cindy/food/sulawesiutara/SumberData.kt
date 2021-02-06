8ec7df29e80a7b183650e2b7c74cd253b08eaff8
Ali Sirait <52443542+Alisirait@users.noreply.github.com>
2021-02-06 13:57:34 +0700

    ninis

--- app/src/main/java/com/cindy/food/sulawesiutara/SumberData.kt
+++ app/src/main/java/com/cindy/food/sulawesiutara/SumberData.kt
@@ -8,83 +8,82 @@ class SumberData {
             val list = ArrayList<ListObjResep>()
             list.add(
                 ListObjResep(
-                    "Ayam Garo Rica \n",
+                    "Mi gobak pedas \n",
                     "Wadah\n" +
                             "Panci",
-                    "https://img-global.cpcdn.com/recipes/6886651adec17aa3/751x532cq70/ayam-garo-rica-khas-manado-sulawesi-utara-foto-resep-utama.webp",
-                    "Bahan-bahan :\n" +
-                            "\uD83D\uDCCCBahan\n" +
-                            "8 potong ayam (sayap & paha bawah)\n" +
-                            "1 buah Jeruk nipis\n" +
-                            "250 ml Air. (bisa ditambah sesuai selera)\n" +
-                            "1 genggam Kemangi\n" +
-                            "2 buah tomat (potong)\n" +
-                            "1 batang sereh (geprek)\n" +
-                            "8 lembar daun jeruk, buang batang nya\n" +
-                            "3 lembar daun salam\n" +
-                            "secukupnya garam (saya 2 sdt)\n" +
-                            "Secukupnya kaldu jamur. (saya 2 sdt)\n" +
-                            "5 sdm minyak goreng\n" +
-                            "\uD83D\uDCCCbumbu halus\n" +
-                            "6 siung bawang putih\n" +
-                            "2 siung bawang merah\n" +
-                            "3 cm jahe\n" +
-                            "1 jempol kunyit\n" +
-                            "2 sdt kunyit bubuk (ini saya tambahkan krn masih kurang kuning)\n" +
-                            "\uD83D\uDCCCbumbu Iris\n" +
-                            "5 cabe rawit\n" +
-                            "5 cabe besar Merah\n" +
-                            "3 cabe besar hijau\n",
-                    "Langkah :\n" +
-                            "- Cuci bersih ayam segar. lalu Rendam ayam dengan Perasan jeruk nipis selama 10 menit. sisihkan.\n" +
-                            "- Sembari menunggu ayam direndam, kita racik bumbu halus dan bumbu iris nya. sisihkan.\n" +
-                            "- Panaskan minyak lalu tumis sereh, daun salam dan daun jeruk nipis hingga harum. Masukkan bumbu halus lalu tumis hingga matang.\n" +
-                            "- Masukkan ayam hingga berubah warna, lalu tambahkan bumbu Iris. tumis hingga bumbu iris matang.. Lalu Tuang air secukupnya.\n" +
-                            "- Tambah garam serta kaldu jamur masak hingga menyusut. Lalu tambahkan kemangi dan tomat..\n" +
-                            "- Masak hingga matang. NB : (Bisa dimasak sedikit berkuah atau di masak hingga air benar benar menyusut. Sesuaikan selera).\n" +
-                            "- Sajikan dengan nasi panas. \n"
-                )
-            )
-            list.add(
-                ListObjResep(
-                    "Bakwan Jagung Daun Mint",
-                    "Kompor",
-                    "https://img-global.cpcdn.com/recipes/d01cb30cf0049b87/751x532cq70/bakwan-jagung-daun-mint-khas-manado-sulawesi-utara-foto-resep-utama.webp",
-                    "Bahan-bahan :\n" +
-                            "1 Buah Jagung manis berukuran besar\n" +
-                            "30 helai Daun Mint\n" +
-                            "70 gram Terigu\n" +
-                            "1 sdt Royco Ayam (bisa diganti garam atau kaldu bubuk merk lain)\n" +
-                            "1/2 sdt Vetsin (optional)\n" +
-                            "Secukupnya Air\n" +
-                            "Secukupnya Minyak untuk menggoreng\n" +
-                            "Bumbu Halus :\n" +
-                            "1 Tangkai Daun Bawang (iris2)\n" +
-                            "3 Siung Bawang Putih (iris2)\n",
-                    "Langkah :\n" +
-                            "- Iris/Pipil Kecil Jagung dan Daun Mint\n" +
-                            "- Blender Bahan Bumbu Halus\n" +
-                            "- Campur Semua Bahan\n" +
-                            "- Goreng adonan sampai habis\n" +
-                            "- Angkat dan Sajikan\n"
-                )
-            )
-            list.add(
-                ListObjResep(
-                    "Kue Paso",
-                    "Kompor",
-                    "http://sulbarkita.com/news-5f6c2601ba3c4.jpg",
-                    "Bahan\n" +
-                            "- ½ ons tepung kanji\n" +
-                            "- 2 ons tepung beras\n" +
-                            "- ½ ons gula merah, serut\n" +
-                            "- 50 cc air daun suji dan pandan\n" +
-                            "- 750 cc santan encer\n" +
-                            "- Garam secukupnya\n" +
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/22/172694/1115329-1000xauto-resep-makanan-khas-sumatera-utara.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 1/2 bungkus mi lidi rebus, tambah sedikit minyak lalu cuci bersih tiriskan\n" +
+                            "- 200 gr kol cuci dan iris-iris\n" +
+                            "- 100 gr wortel cuci dan iris-iris\n" +
+                            "- Daun bawang, iris-iris\n" +
+                            "- Seledri, iris-iris\n" +
+                            "- 7 butir bawang merah, iris\n" +
+                            "- Cabai secukupnya\n" +
+                            "- Penyedap rasa\n" +
+                            "- Garam\n" +
+                            "- Gula\n" +
+                            "- Kecap sesuai selera\n" +
                             "\n" +
-                            "Toping\n" +
-                            "- 2 sdm tepung kanji\n" +
-                            "- 200 cc santan kental dari 1 kelapa\n",
+                            "Bumbu halus:\n" +
+                            "- 2 sdt merica atau sesuaikan selera\n" +
+                            "- 2 buah kemiri\n" +
+                            "- 5 siung bawang putih\n",
+                    "Cara membuat:\n" +
+                            "- Tumis bawang merah sampai harum. Masukkan bumbu halus tumis sampai harum pula\n" +
+                            "- Masukkan wortel kol daun bawang tumis sampai layu masukkan sedikit air cabai hijau, iris dan aduk\n" +
+                            "- Masukkan mi lidinya tambahkan garam, gula, penyedap rasa dan kecap manis, lalu aduk rata\n" +
+                            "- Setelah tanak tes rasa dan masukkan rawit utuh dan daun seledri, aduk dan sajikan.\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Daun ubi tumbuk",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/22/172694/1115327-1000xauto-resep-makanan-khas-sumatera-utara.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 1 ikat daun singkong ambil bagian pucuknya\n" +
+                            "- 1 buah kincung (kecombrang), belah 2\n" +
+                            "- Segenggam rimbang\n" +
+                            "- 6 butir bawang merah\n" +
+                            "- 3 siung bawang putih\n" +
+                            "- 2 ruas jari jahe\n" +
+                            "- 1 ruas jari kunyit\n" +
+                            "- 1 ruas jari lengkuas geprek\n" +
+                            "- 2 lembar daun salam\n" +
+                            "- 1 batang serai ambil bagian putih geprek\n" +
+                            "- 5 buah cabai rawit atau sesuai selera\n" +
+                            "- 1 ons teri asin\n" +
+                            "- 400 ml santan sedang\n",
+                    "Cara membuat:\n" +
+                            "- Haluskan cabai, bawang merah, bawang putih, kunyit, jahe\n" +
+                            "- Setelah agak halus, tumbuk daun singkong dan kincung (kecombrang) bersama bumbu, tumbuk setengah halus sampai rata\n" +
+                            "- Panaskan air di panci, masukkan tumbukan daun singkong beserta bumbu dan teri asin\n" +
+                            "- Masukkan serai, daun salam dan lengkuas geprek\n" +
+                            "- Masak sampai mendidih, masukkan santan\n" +
+                            "- Terakhir koreksi rasa dengan gula dan garam\n" +
+                            "- Sajikan\n"
+                )
+            )
+            list.add(
+                ListObjResep(
+                    "Oseng teri Medan",
+                    "Kompor",
+                    "https://cdn-brilio-net.akamaized.net/news/2019/10/22/172694/1115328-1000xauto-resep-makanan-khas-sumatera-utara.jpg",
+                    "Bahan-bahan:\n" +
+                            "- 1/4 kg teri Medan\n" +
+                            "- 2 lonjor petai\n" +
+                            "- Gula merah secukupnya\n" +
+                            "\n" +
+                            "Bumbu iris:\n" +
+                            "- 8 siung bawang putih\n" +
+                            "- 5 butir bawang merah\n" +
+                            "- Cabai rawit sesuai selera\n" +
+                            "\n" +
+                            "Bumbu geprek\n" +
+                            "- 2 lembar daun salam\n" +
+                            "- 2 cm jahe\n" +
+                            "- 2 lembar daun jeruk\n",
                     "Cara Membuat\n" +
                             "- Jadikan satu tepung beras, tepung kanji, garam dan air suji sampai benar-benar tercampur merata.\n" +
                             "- Sediakan mangkok plastik kecil atau takir daun pisang.\n" +
